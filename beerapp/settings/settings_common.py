d12e72ea8896ef6a283e3d34000756e03aeb9e28
Filipe Saraiva <mail@filipesaraiva.info>
2015-11-09 18:26:26 -0200

    Altera configurações do feed para TeresinaHC
    Altera configurações que identificam o feed do BeerBlogging, como título
    e link do feed, para configurações que identificam o Planet TeresinaHC.

--- beerapp/settings/settings_common.py
+++ beerapp/settings/settings_common.py
@@ -1,7 +1,7 @@
 PAGINATION_ITEMS_PER_PAGE = 5
 
-FEED_TITLE = "BeerBlogging"
-FEED_LINK = "http://www.beerblogging.io"
-FEED_AUTHOR = "BeerBlogging Team"
-FEED_DESC = "Posts from all beerbloggers"
+FEED_TITLE = "Planet TeresinaHC"
+FEED_LINK = "http://planet.teresinahc.org"
+FEED_AUTHOR = "Planet TeresinaHC Team"
+FEED_DESC = "Posts from all TeresinaHC hackers"
 FEED_ITEMS = 10
